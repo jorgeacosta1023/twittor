6764d6c62289f1d431ab5559c0b265ef27fb2183
Jorge Ramirez <jaramireza@frd.org.mx>
2019-02-26 17:28:35 -0600

    segundo commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 // Service Worker
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
 
 
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('sw.js');
+
+    if (url.includes('localhost')){
+        swLocation= '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
