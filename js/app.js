d3af09e3bf400a6b8fe3bb3a895ac29c9447c875
Jorge Ramirez <jaramireza@frd.org.mx>
2019-03-13 16:39:29 -0600

    HOMESCREEN

--- js/app.js
+++ js/app.js
@@ -13,7 +13,18 @@ if(navigator.serviceWorker){
     navigator.serviceWorker.register(swLocation);
 }
 
-
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+  
+  // Checks if should display install popup notification:
+  if (isIos() && !isInStandaloneMode()) {
+    this.setState({ showInstallMessage: true });
+  } 
 // Referencias de jQuery
 
 var titulo      = $('#titulo');
